bf74cf146a3f7a6b4197b6c0ba6c849535e04704
André Dargains <andre.dargains@gmail.com>
2023-04-23 12:54:18 +0100

    slideup

--- src/js/landing.js
+++ src/js/landing.js
@@ -12,6 +12,16 @@ createApp({
     images.forEach(image => {
       new Parallax(image);
     })
+
+    const slideUps = document.querySelectorAll(".slide-up")
+
+    slideUps.forEach(element => {
+      window.addEventListener('scroll', () => {
+        if (this.isElementInViewport(element)) {
+          element.classList.add('active');
+        }
+      });
+    })
   },
   computed: {
 
@@ -20,6 +30,15 @@ createApp({
     scrolldown() {
       const nextElement = document.querySelector('.infoBlock')
       nextElement.scrollIntoView({ behavior: 'smooth' })
-    }
+    },
+    isElementInViewport(el) {
+      var rect = el.getBoundingClientRect();
+      return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      );
+    },
   }
 }).mount('#landing')
